5dc75c436ea8f5cb2ae3223bc102b1beda875faf
Ritesh Kumar Prasad <92202038+1Ritesh326@users.noreply.github.com>
2023-08-11 19:10:59 +0530

    Add files via upload
    first commit

---
+++ Billo'sSkillAcademy/src/main/java/com/example/demo/services/StudentServiceImplementation.java
@@ -0,0 +1,22 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Lesson;
+import com.example.demo.repository.LessonRepository;
+import com.example.demo.repository.UserRepository;
+@Service
+public class StudentServiceImplementation implements StudentServices{
+	@Autowired
+	UserRepository ur;
+	@Autowired
+	LessonRepository lessonRepo;
+	@Override
+	public Lesson getLesson(int lessonId) {
+		
+		return lessonRepo.findById(lessonId).get();
+		
+	}
+
+}
